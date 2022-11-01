a7eef15fd972c6ae39332999c103df8f81d7b22d
Fidelis Wandia <fidelismundia2000@gmail.com>
2022-11-01 07:36:00 +0300

    profile section

---
+++ link-tree/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div id="container">
+      <section id="profile">
+        <div>
+        <img id="profile__img" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=400"/>
+        </div>
+        <div>
+        <p id="twitter">wandia_mundia</p>
+        {/* <p id="slack">Fidelis Mundia</p> */} 
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App;
